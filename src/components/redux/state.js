93c9b62c4f85f3ac5929ad86582ef6389e9c9dad
Gennadii Rostikov <107649327+GeshaCanada@users.noreply.github.com>
2022-09-24 12:47:19 -0400

    store (Redux state menegment)

--- src/components/redux/state.js
+++ src/components/redux/state.js
@@ -1,51 +1,66 @@
-import { rerenderEntireTree } from "../../render";
-
-let state = {
-
-    profilePage: {
-        posts: [
-            { id: 1, message: "What's up?", likeCount: 0 },
-            { id: 2, message: "It's my first post", likeCount: 23 },
-            { id: 3, message: "Realy?", likeCount: 1 },
-            { id: 4, message: "Yep!", likeCount: 11 }
-        ],
-        newPostText:'React is cool!'
+let store = {                            // store OOP
+   
+    getState() {
+        return this._state
     },
-    dialogsPage: {
-        dialogs: [
-            { id: 1, name: 'Gennadii' },
-            { id: 2, name: 'Max' },
-            { id: 3, name: 'Anastasia' },
-            { id: 4, name: 'Victoria' },
-            { id: 5, name: 'Daniel' },
-            { id: 6, name: 'Sofia' }
-        ],
-        messages: [
-            { id: 1, message: 'Hello!' },
-            { id: 2, message: 'How is going your React project?' },
-            { id: 3, message: 'Yoo))' },
-            { id: 4, message: 'Yoo))' },
-            { id: 5, message: 'Yoo))' },
-            { id: 6, message: 'Yoo))' }
-        ]
+
+    _state: {
+
+        profilePage: {
+            posts: [
+                { id: 1, message: "What's up?", likeCount: 0 },
+                { id: 2, message: "It's my first post", likeCount: 23 },
+                { id: 3, message: "Realy?", likeCount: 1 },
+                { id: 4, message: "Yep!", likeCount: 11 }
+            ],
+            newPostText: 'React is cool!'
+        },
+        dialogsPage: {
+            dialogs: [
+                { id: 1, name: 'Gennadii' },
+                { id: 2, name: 'Max' },
+                { id: 3, name: 'Anastasia' },
+                { id: 4, name: 'Victoria' },
+                { id: 5, name: 'Daniel' },
+                { id: 6, name: 'Sofia' }
+            ],
+            messages: [
+                { id: 1, message: 'Hello!' },
+                { id: 2, message: 'How is going your React project?' },
+                { id: 3, message: 'Yoo))' },
+                { id: 4, message: 'Yoo))' },
+                { id: 5, message: 'Yoo))' },
+                { id: 6, message: 'Yoo))' }
+            ]
+        }
+    },
+    
+    _callSubscriber() {
+        console.log('state changed')
+    },
+    
+    addPost() {
+        let newPost = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            likeCount: 0
+        }
+        this._state.profilePage.posts.push(newPost)
+        this._state.profilePage.newPostText = ''
+        this._callSubscriber(this._state)
+    },
+
+    updateNewPostText(newText) {
+        this._state.profilePage.newPostText = newText
+        this._callSubscriber(this._state)
+    },
+
+    subscribe(observer) {
+        this._callSubscriber = observer         // observer patern
     }
+
 }
 
-window.state=state
+export default store
 
-export let addPost = () => {
-    let newPost = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        likeCount: 0
-    }
-    state.profilePage.posts.push(newPost)
-    state.profilePage.newPostText = ''
-    rerenderEntireTree(state)
-}
-
-export let updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText
-    rerenderEntireTree(state)
-}
-export default state
+window.store = store
--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import store from './components/redux/state';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
-import { rerenderEntireTree } from './render';
-import state from './components/redux/state';
 
 
-rerenderEntireTree(state);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+let rerenderEntireTree = (state) => {
+  root.render(
+    <BrowserRouter>
+      <React.StrictMode>
+        <App state={state} addPost={store.addPost.bind(store)} 
+             updateNewPostText={store.updateNewPostText.bind(store)} />
+      </React.StrictMode>
+    </BrowserRouter>
+  );
+}
+rerenderEntireTree(store.getState());
+
+store.subscribe(rerenderEntireTree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
